1c164190b873b941dc547c77d6a86162425ec1dc
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-04-30 21:16:30 +0100

    implementing the fake authentication feature

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -1,11 +1,12 @@
 import { createContext, useContext, useReducer } from "react";
 
-const AuthContext = createContext;
+const AuthContext = createContext();
 
 const initialState = {
   user: null,
   isAuthenticated: false,
 };
+
 function reducer(state, action) {
   switch (action.type) {
     case "login":
@@ -37,14 +38,17 @@ function AuthProvider({ children }) {
     reducer,
     initialState
   );
+
   function login(email, password) {
     if (email === FAKE_USER.email && password === FAKE_USER.password) {
       dispatch({ type: "login", payload: FAKE_USER });
     }
   }
+
   function logout() {
     dispatch({ type: "logout" });
   }
+
   return (
     <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
       {children}
